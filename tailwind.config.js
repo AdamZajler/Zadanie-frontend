7427c7de4cc1fe4107b9ceac8eb4954079a156ae
Adam Zajler <adamzajler01@gmail.com>
2022-04-21 19:19:38 +0200

    Init

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	content: ["./src/**/*.{js,ts,jsx,tsx}"],
+	mode: "jit",
+	theme: {
+		extend: {
+			colors: {
+				primary: "#05867e",
+				gray: "#F1F2F6",
+			},
+		},
+	},
+	plugins: [],
+};
